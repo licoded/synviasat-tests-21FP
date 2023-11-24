45ab142e9c62e29589f858f18e47d2854b25d1b4
licoded <busy.li@foxmail.com>
2023-11-24 11:36:20 +0800

    test(onechecker): initialize

---
+++ tests/onechecker/01.basic.cpp
@@ -0,0 +1,35 @@
+#include "onechecker.h"
+#include "formula/aalta_formula.h"
+#include <iostream>
+
+using namespace std;
+using namespace aalta;
+
+int main()
+{
+    string input_f; // = "a U b";
+	cin >> input_f;
+
+	// rewrite formula
+	aalta_formula *af;
+	// set tail id to be 1
+	af = aalta_formula::TAIL();
+	aalta_formula::TRUE();
+	aalta_formula::FALSE();
+	af = aalta_formula(input_f.c_str(), true).nnf();
+	// af = af->remove_wnext();
+	af = af->simplify();
+	af = af->split_next();
+	af = af->unique();
+
+    OneChecker checker(af, false, true);
+    bool check_res = checker.check(af);
+    auto XY_edge = checker.get_model_for_synthesis();
+    cout
+        << "X:" << "\t" << XY_edge.first->to_string()
+        << endl
+        << "Y:" << "\t" << XY_edge.second->to_string()
+        << endl;
+
+    return 0;
+}
