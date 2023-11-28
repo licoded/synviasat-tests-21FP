f44218ea591315f95d8c3d44e09ba4601e8ff4c1
licoded <busy.li@foxmail.com>
2023-11-28 18:53:32 +0800

    fix-test(onechecker): do partition for X\Y vars

--- tests/onechecker/01.basic.cpp
+++ tests/onechecker/01.basic.cpp
@@ -1,6 +1,8 @@
 #include "onechecker.h"
+#include "synthesis.h"
 #include "formula/aalta_formula.h"
 #include <iostream>
+#include <sstream>
 
 using namespace std;
 using namespace aalta;
@@ -9,6 +11,21 @@ int main()
 {
     string input_f; // = "a U b";
 	getline(cin, input_f);
+
+	string env_vars;
+	getline(cin, env_vars);
+	unordered_set<string> env_var;
+    istringstream iss(env_vars);
+    string token;
+
+    while (std::getline(iss, token, ' ')) {
+        env_var.insert(token);
+    }
+
+	// Printing the elements in the unordered_set
+    // for (const auto& element : env_var) {
+    //     std::cout << element << std::endl;
+    // }
 
 	// rewrite formula
 	aalta_formula *af;
@@ -22,13 +39,15 @@ int main()
 	af = af->split_next();
 	af = af->unique();
 
+	PartitionAtoms(af, env_var);
+
     OneChecker checker(af, false, true);
     bool check_res = checker.check(af);
     auto XY_edge = checker.get_model_for_synthesis();
     cout
-        << "X:" << "\t" << XY_edge.first->to_string()
+        << "X:" << "\t" << XY_edge.second->to_string()
         << endl
-        << "Y:" << "\t" << XY_edge.second->to_string()
+        << "Y:" << "\t" << XY_edge.first->to_string()
         << endl;
 
     return 0;
